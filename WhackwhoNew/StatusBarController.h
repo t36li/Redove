6ab5a0a35afc959c4d8228e0cae531f1aef3e9b4
Bob Li <bobli@Bobs-MacBook-Air.local>
2012-06-22 18:34:05 -0400

    Designed Overall UI Flow

---
+++ WhackwhoNew/StatusBarController.h
@@ -0,0 +1,19 @@
+//
+//  StatusBarController.h
+//  WhackwhoNew
+//
+//  Created by Bob Li on 12-06-22.
+//  Copyright (c) 2012 Waterloo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface StatusBarController : UIViewController {
+    IBOutlet UIImageView *Bobhead;
+}
+
+@property (nonatomic, retain) IBOutlet UIImageView *Bobhead;
+
+
+@end
---
+++ WhackwhoNew/StatusBarController.m
@@ -0,0 +1,55 @@
+//
+//  StatusBarController.m
+//  WhackwhoNew
+//
+//  Created by Bob Li on 12-06-22.
+//  Copyright (c) 2012 Waterloo. All rights reserved.
+//
+
+#import "StatusBarController.h"
+
+
+@implementation StatusBarController
+
+@synthesize Bobhead;
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+    [self.view setBackgroundColor:[UIColor whiteColor]];
+	// Do any additional setup after loading the view, typically from a nib.
+    Bobhead.animationImages = [NSArray arrayWithObjects:[UIImage imageNamed:@"bob1.png"], [UIImage imageNamed:@"bob2.png"], [UIImage imageNamed:@"bob3.png"], nil];
+    Bobhead.animationDuration = 0.5;
+    Bobhead.animationRepeatCount = 0;
+    
+    [Bobhead setContentMode:UIViewContentModeScaleAspectFit];
+    [Bobhead setBackgroundColor:[UIColor clearColor]];
+    [Bobhead startAnimating];
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
+        return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+    } else {
+        return UIInterfaceOrientationIsLandscape(interfaceOrientation);
+    }
+}
+
+@end
