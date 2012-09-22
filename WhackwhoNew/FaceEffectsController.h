274ee3e0daa726fc6f7b0ce73e2a51623fcff8ab
Peter Xu <peterxu333@hotmail.com>
2012-09-22 14:01:01 -0400

    fixed bug of photo not updating when going from avastarview to statusbarview.

---
+++ WhackwhoNew/FaceEffectsController.h
@@ -0,0 +1,13 @@
+//
+//  FaceEffectsController.h
+//  WhackwhoNew
+//
+//  Created by Peter on 2012-09-22.
+//  Copyright (c) 2012 Waterloo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FaceEffectsController : UIViewController
+
+@end
---
+++ WhackwhoNew/FaceEffectsController.m
@@ -0,0 +1,44 @@
+//
+//  FaceEffectsController.m
+//  WhackwhoNew
+//
+//  Created by Peter on 2012-09-22.
+//  Copyright (c) 2012 Waterloo. All rights reserved.
+//
+
+#import "FaceEffectsController.h"
+
+@interface FaceEffectsController ()
+
+@end
+
+@implementation FaceEffectsController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
