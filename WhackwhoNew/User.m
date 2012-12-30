14ba3c4fc4f99b602bbad7e75b844853471b09de
chun su <chunsu@chuns-MacBook-Pro.local>
2012-12-29 22:52:40 -0500

    nosePosition, leftEarPosition, rightEarPosition added

--- WhackwhoNew/User.m
+++ WhackwhoNew/User.m
@@ -12,6 +12,7 @@
 
 @implementation User
 @synthesize mediaTypeId,whackWhoId,headId,mediaKey,leftEyePosition,rightEyePosition,mouthPosition,faceRect,registeredDate, userImgURL, currentEquip, storageInv,popularity;
+@synthesize nosePosition,leftEarPosition,rightEarPosition;
 
 -(void)copyToUserInfo{
     UserInfo *usrInfo = [UserInfo sharedInstance];
@@ -22,9 +23,17 @@
     [usrInfo setLeftEyePosition:CGPointFromString(leftEyePosition)];
     [usrInfo setRightEyePosition:CGPointFromString(rightEyePosition)];
     [usrInfo setMouthPosition:CGPointFromString(mouthPosition)];
+    [usrInfo setNosePosition:CGPointFromString(nosePosition)];
+    [usrInfo setRightEarPosition:CGPointFromString(rightEarPosition)];
+    [usrInfo setLeftEarPosition:CGPointFromString(leftEarPosition)];
     [usrInfo setFaceRect:CGRectFromString(faceRect)];
+    
+    /*   Current Equip and Storage NEEDED IN THE FUTURE!!! NO DELETE!
+     
     [usrInfo setCurrentEquip:[currentEquip currentEquipInFileNames]];
     [usrInfo setStorageInv:[storageInv setStorageArrayInFileNames]];
+     
+     */
     [usrInfo setPopularity:popularity];
     usrInfo->usrImgURL = userImgURL;
     
@@ -51,14 +60,24 @@
     leftEyePosition = NSStringFromCGPoint(usrInfo.leftEyePosition);
     rightEyePosition = NSStringFromCGPoint(usrInfo.rightEyePosition);
     mouthPosition = NSStringFromCGPoint(usrInfo.mouthPosition);
+    nosePosition = NSStringFromCGPoint(usrInfo.nosePosition);
+    leftEarPosition = NSStringFromCGPoint(usrInfo.leftEarPosition);
+    rightEarPosition = NSStringFromCGPoint(usrInfo.rightEarPosition);
     faceRect = NSStringFromCGRect(usrInfo.faceRect);
+    
+    /*     Current Equip and Storage NEEDED IN THE FUTURE!!! NO DELETE!
+     
     currentEquip = [usrInfo.currentEquip currentEquipInIDs];
     storageInv = [usrInfo.storageInv setStorageStringInIDs];
+     */
+    
     userImgURL = usrInfo->usrImgURL;
     
 }
 
 +(void)objectMappingLoader{
+    /*    Current Equip and Storage NEEDED IN THE FUTURE!!! NO DELETE!
+     
     //currentEquip mapping:
     RKObjectMapping *curEquipMapping = [RKObjectMapping mappingForClass:[CurrentEquip class]];
     [curEquipMapping mapKeyPath:@"helmet" toAttribute:@"helmet"];
@@ -75,6 +94,7 @@
     [storageInvMapping mapKeyPath:@"HammerArms" toAttribute:@"hammerArms"];
     [storageInvMapping mapKeyPath:@"ShieldArms" toAttribute:@"shieldArms"];
     [[RKObjectManager sharedManager].mappingProvider setMapping:storageInvMapping forKeyPath:@"storageInv"];
+    */
     
     //head Mapping:
     RKObjectMapping *headMapping = [RKObjectMapping mappingForClass:[Head class]];
@@ -96,7 +116,10 @@
     [friendInfoMapping mapKeyPath:@"gender" toAttribute:@"gender"];
     [friendInfoMapping mapKeyPath:@"hits" toAttribute:@"popularity"];
     [friendInfoMapping mapKeyPath:@"head" toRelationship:@"head" withMapping:headMapping];
+    /*    Current Equip and Storage NEEDED IN THE FUTURE!!! NO DELETE!
+     
     [friendInfoMapping mapKeyPath:@"currentEquip" toRelationship:@"currentEquip" withMapping:curEquipMapping];
+     */
     [[RKObjectManager sharedManager].mappingProvider setMapping:friendInfoMapping forKeyPath:@"friend"];
     
     //friend array mapping:
@@ -123,12 +146,17 @@
     [userInfoMapping mapKeyPath:@"leftEyePosition" toAttribute:@"leftEyePosition"];
     [userInfoMapping mapKeyPath:@"rightEyePosition" toAttribute:@"rightEyePosition"];
     [userInfoMapping mapKeyPath:@"mouthPosition" toAttribute:@"mouthPosition"];
+    [userInfoMapping mapKeyPath:@"nosePosition" toAttribute:@"nosePosition"];
+    [userInfoMapping mapKeyPath:@"leftEarPosition" toAttribute:@"leftEarPosition"];
+    [userInfoMapping mapKeyPath:@"rightEarPosition" toAttribute:@"rightEarPosition"];
     [userInfoMapping mapKeyPath:@"faceRect" toAttribute:@"faceRect"];
     [userInfoMapping mapKeyPath:@"userImgURL" toAttribute:@"userImgURL"];
     [userInfoMapping mapKeyPath:@"hits" toAttribute:@"popularity"];
+    /*   Current Equip and Storage NEEDED IN THE FUTURE!!! NO DELETE!
+     
     [userInfoMapping mapKeyPath:@"currentEquip" toRelationship:@"currentEquip" withMapping:curEquipMapping];
     [userInfoMapping mapKeyPath:@"storageInv" toRelationship:@"storageInv" withMapping:storageInvMapping];
-    
+     */    
     [[RKObjectManager sharedManager].mappingProvider setMapping:userInfoMapping forKeyPath:@"user"];
     //[RKObjectManager sharedManager].serializationMIMEType = RKMIMETypeJSON;
     [[RKObjectManager sharedManager].mappingProvider setSerializationMapping:[userInfoMapping inverseMapping] forClass:[User class]];
