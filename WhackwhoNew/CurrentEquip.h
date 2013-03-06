48dc99aef0d1cd22c83c3605c708732da391275d
Peter Xu <peterxu333@hotmail.com>
2013-03-06 17:32:13 -0500

    commit

--- WhackwhoNew/CurrentEquip.h
+++ WhackwhoNew/CurrentEquip.h
@@ -7,7 +7,7 @@
 //
 
 #import <Foundation/Foundation.h>
-#import <RestKit/RestKit.h>
+//#import <RestKit/RestKit.h>
 
 @interface CurrentEquip : NSObject{
     NSString *helmet, *hammerArm, *shieldArm, *body;
